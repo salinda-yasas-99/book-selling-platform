65afe50db2992ccfd78631a46c0984db6b405752
salinda-yasas-99 <salindasym@gmail.com>
2023-07-23 09:51:09 +0530

    adding tempalte engines(ejs)

--- app.js
+++ app.js
@@ -2,20 +2,11 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: "views/layouts/",
-    defaultLayout: "main-layout",
-    extname: "hbs",
-  })
-);
 // app.engine("hbs", expressHbs({ extname: "hbs" }));
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
@@ -28,7 +19,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { docTitle: "Error" });
+  res.status(404).render("404", { docTitle: "Error", path: "" });
 });
 
 app.listen(3000);
